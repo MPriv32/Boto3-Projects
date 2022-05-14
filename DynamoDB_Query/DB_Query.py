b372bda80a0ba10697a8e7856b6a03e4accc0cec
MITCHELL PRIVETT <lahc33@gmail.com>
2022-05-13 18:01:58 -0700

    Updated DB script

--- DynamoDB_Query/DB_Query.py
+++ DynamoDB_Query/DB_Query.py
@@ -1,11 +1,23 @@
 import boto3, json
 from boto3.dynamodb.conditions import Key
 
+
+#Calling DynamoDB module and assigning the table name
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('stock_tracker')
 
+#Querying the primary key of the table
 resp = table.query(KeyConditionExpression=Key('Company').eq('AAPL'))
 
-print("The query returned the following items:")
+#Assigning the item count and total price to a variable in order to find the average daily price
+numItems = resp['Count']
+total_price = 0
+
 for item in resp['Items']:
-    print(json.dumps(item, indent=4, sort_keys=True))
+    # print(json.dumps(item, indent=4, sort_keys=True))
+    price_per_day = float(item['DailyPrice'])
+    total_price += price_per_day
+
+average_price = total_price / numItems
+
+print(format(average_price, '.2f'))
